ebb0f2588944aa119556a4cd1dd580e02755a17f
huaileicoder <32975696+huaileicoder@users.noreply.github.com>
2019-12-25 19:43:22 +0800

    remove isEquals method

--- AnagramTest.java
+++ AnagramTest.java
@@ -1,6 +1,7 @@
 package leetcode.LeetCode;
 
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class AnagramTest {
     public static void main(String[] args) {
@@ -9,46 +10,52 @@ public class AnagramTest {
         System.out.println(isAnagram(s, t));
     }
 
+    //private static boolean isAnagram(String s, String t) {
+    //    if (null == s || null == t || s.length() != t.length()) {
+    //        return false;
+    //    }
+    //    Map<Integer, Character> map1 = new TreeMap<>();
+    //    Map<Integer, Character> map2 = new TreeMap<>();
+    //    putToMap(s, map1);
+    //    putToMap(t, map2);
+    //    return map1.equals(map2);
+    //}
+    //
+    //private static void putToMap(String s, Map<Integer, Character> map) {
+    //    char[] chars = s.toCharArray();
+    //    Arrays.sort(chars);
+    //    int index = 0;
+    //    for (char c : chars) {
+    //        map.put(index, c);
+    //        index++;
+    //    }
+    //}
+
     private static boolean isAnagram(String s, String t) {
         if (null == s || null == t || s.length() != t.length()) {
             return false;
         }
         char[] chars1 = s.toCharArray();
         char[] chars2 = t.toCharArray();
-        Map<Character, Integer> characters1 = new HashMap<>();
-        Map<Character, Integer> characters2 = new HashMap<>();
-        for (char c : chars1) {
-            Integer oldNum = characters1.get(c);
-            if (null == oldNum) {
-                characters1.put(c, 1);
-            } else {
-                characters1.put(c, oldNum + 1);
+        Map<Character, Integer> characters1 = new HashMap<>(chars1.length);
+        Map<Character, Integer> characters2 = new HashMap<>(chars2.length);
+        BiFunction<Integer, Integer, Integer> biFunction = new BiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) {
+                return integer + integer2;
             }
+        };
+        for (char c : chars1) {
+            characters1.merge(c, 1, biFunction);
         }
         for (char c : chars2) {
-            Integer oldNum = characters2.get(c);
-            if (null == oldNum) {
-                characters2.put(c, 1);
-            } else {
-                characters2.put(c, oldNum + 1);
-            }
+            characters2.merge(c, 1, biFunction);
         }
-
         if (characters1.size() != characters2.size()) {
             return false;
         }
 
-        return isEquals(characters1, characters2);
+        return characters1.equals(characters2);
     }
 
-    private static boolean isEquals(Map<Character, Integer> c1, Map<Character, Integer> c2) {
-        for (Map.Entry<Character, Integer> entry : c1.entrySet()) {
-            Character character = entry.getKey();
-            Integer c2Num = c2.get(character);
-            if (null == c2Num || !c2Num.equals(entry.getValue())) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
