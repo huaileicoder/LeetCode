fce2631e5370a95d5e117b9879739c477d8b95e6
huaileicoder <HL_0906n@163.com>
2020-04-08 09:18:30 +0800

    add CountOfCoverDistance

---
+++ CountOfCoverDistance.java
@@ -0,0 +1,25 @@
+package leetcode.LeetCode;
+
+public class CountOfCoverDistance {
+    public static void main(String[] args) {
+        System.out.println(count(6));
+    }
+
+    private static int count(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int[] count = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                count[i] = 1;
+            }
+            for (int j = 1; j <= 3; j++) {
+                if (i >= j) {
+                    count[i] += count[i - j];
+                }
+            }
+        }
+        return count[n];
+    }
+}
