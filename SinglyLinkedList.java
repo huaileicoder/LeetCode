29e4c626916b96e29c7ebd324c3b692ce5c46554
huaileicoder <HL_0906n@163.com>
2020-04-22 14:35:50 +0800

    add linked list check cycle and merge two sorted linked list

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -59,17 +59,7 @@ public class SinglyLinkedList<E> {
     }
 
     public E removeFirst() {
-        final Node<E> h = head;
-        if (null == h) {
-            throw new NoSuchElementException();
-        }
-        head = h.next;
-        if (null == head) {
-            tail = null;
-        }
-        h.next = null;
-        size--;
-        return h.item;
+        return removeFirstNode().item;
     }
 
     public E removeLast() {
@@ -87,6 +77,20 @@ public class SinglyLinkedList<E> {
         return r.item;
     }
 
+    Node<E> removeFirstNode() {
+        final Node<E> h = head;
+        if (null == h) {
+            throw new NoSuchElementException();
+        }
+        head = h.next;
+        if (null == head) {
+            tail = null;
+        }
+        h.next = null;
+        size--;
+        return h;
+    }
+
     public E getFirst() {
         final Node<E> h = head;
         if (null == h) {
@@ -101,6 +105,14 @@ public class SinglyLinkedList<E> {
             throw new NoSuchElementException();
         }
         return l.item;
+    }
+
+    public boolean isEmpty() {
+        return null == head;
+    }
+
+    public boolean isNotEmpty() {
+        return !isEmpty();
     }
 
     public void printAll() {
@@ -135,6 +147,80 @@ public class SinglyLinkedList<E> {
             leftLink = inverse(p);
         }
         return isEquals(leftLink, rightLink);
+    }
+
+
+    /**
+     * solution1: 遍历链表，把遍历的每一个节点作为key放进map中，如果put() return is not null, 就是一个环，但是这种方法需要node重写hash和
+     * equals方法，判断equal比较麻烦，time complexity O(n^2)
+     *
+     * solution2: node存一个flag，遍历链表，经过节点即设为true，如果遍历的时候node flag is true，则就是环，这种需要每个节点多存一些数据
+     *
+     * solution3: 快慢节点，如果快慢节点相同则是环
+     *
+     * solution4: 2的改进版，node不存flag，增加一个Temp Node，每遍历一个节点把这个几点next指向temp，如果current node 的next指向temp
+     * 说明成环，但是这一种会破坏链表结构
+     *
+     * 这里推荐1和3，但是1需要注意equals方法有没有好的处理方式
+     */
+    public boolean isCycle() {
+        if (null == head || null == head.next) {
+            return false;
+        }
+        Node<E> p = head;
+        Node<E> q = head;
+        while (null != q.next && null != q.next.next) {
+            p = p.next;
+            q = q.next.next;
+            if (p == q) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public SinglyLinkedList<E> mergeOrderlyList(SinglyLinkedList<E> orderlyLinkedList) {
+        if (orderlyLinkedList.isEmpty()) {
+            return this;
+        }
+        Node<E> h = head;
+        Node<E> pre = null;
+        Node<E> next = h.next;
+        Node<E> h2 = orderlyLinkedList.removeFirstNode();
+        while (null != h) {
+            if ((Integer) h2.item <= (Integer) h.item) {
+                if (null == pre) {
+                    h2.next = h;
+                    head = h2;
+                } else {
+                    h2.next = h;
+                    pre.next = h2;
+                }
+                h2 = orderlyLinkedList.removeFirstNode();
+                if (null == h2) {
+                    break;
+                }
+            }
+            pre = h;
+            h = next;
+            if (null != next) {
+                next = next.next;
+            }
+        }
+        if (null != h2) {
+            pre.next = h2;
+        }
+        while (orderlyLinkedList.isNotEmpty()) {
+            Node<E> h3 = orderlyLinkedList.removeFirstNode();
+            tail.next = h3;
+            tail = h3;
+        }
+
+        return this;
+    }
+
+    public void setCycle() {
+        tail.next = head;
     }
 
     public int getSize() {
--- test/SinglyLinkedListTest.java
+++ test/SinglyLinkedListTest.java
@@ -8,16 +8,33 @@ import java.util.List;
 
 public class SinglyLinkedListTest {
     public static void main(String[] args) {
-        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>(getList());
-        System.out.println("是否是回文，" + linkedList.isPalindrome());
+        /*SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>(getList());
+        linkedList.setCycle();
+        System.out.println("is cycle，" + linkedList.isCycle());*/
+
+        SinglyLinkedList<Integer> linkedList1 = new SinglyLinkedList<>(getList());
+        SinglyLinkedList<Integer> linkedList2 = new SinglyLinkedList<>(getList2());
+        linkedList1.mergeOrderlyList(linkedList2);
+        linkedList1.printAll();
+
     }
 
     private static Collection<Integer> getList() {
         List<Integer> list = new ArrayList<>();
-        list.add(2);
-        list.add(3);
-        list.add(3);
         list.add(1);
+        list.add(3);
+        list.add(5);
         return list;
     }
+
+    private static Collection<Integer> getList2() {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(4);
+        list.add(5);
+        return list;
+    }
+
+
+
 }
