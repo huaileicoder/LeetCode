2110b2a5dd25af9a063ca83f8a67d255671748a7
huaileicoder <HL_0906n@163.com>
2020-06-06 20:48:07 +0800

    add CircularArrayQueue

---
+++ datastructure/CircularArrayQueue.java
@@ -0,0 +1,39 @@
+package leetcode.datastructure;
+
+public class CircularArrayQueue<E> implements Queue<E> {
+    private Object[] items;
+    private int capacity;
+    private int head;
+    private int tail;
+
+    public CircularArrayQueue(int capacity) {
+        this.capacity = capacity;
+        items = new Object[capacity];
+    }
+
+    @Override
+    public void enQueue(E e) {
+        if (head == (tail + 1) % capacity) {
+            throw new ArrayIndexOutOfBoundsException("head" + head + ", tail=" + tail);
+        }
+        items[tail] = e;
+        tail = (tail + 1) % capacity;
+    }
+
+    @Override
+    public E deQueue() {
+        if (head == tail) {
+            return null;
+        }
+        E e =  (E) items[head];
+        head = (head + 1) % capacity;
+        return e;
+    }
+
+    public void printAll() {
+        for (int i = head; i < tail; i++) {
+            System.out.print(items[i]);
+        }
+        System.out.println();
+    }
+}
--- test/ArrayQueueTest.java
+++ test/ArrayQueueTest.java
@@ -1,22 +1,19 @@
 package leetcode.test;
 
-import leetcode.datastructure.ArrayQueue;
+import leetcode.datastructure.CircularArrayQueue;
 
 public class ArrayQueueTest {
     public static void main(String[] args) {
-        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>(5);
-        arrayQueue.enQueue(1);
-        arrayQueue.enQueue(2);
-        arrayQueue.enQueue(3);
-        arrayQueue.printAll();
-        print(arrayQueue.deQueue());
-        print(arrayQueue.deQueue());
-        arrayQueue.enQueue(4);
-        arrayQueue.enQueue(5);
-        arrayQueue.enQueue(6);
-        arrayQueue.enQueue(6);
-        arrayQueue.enQueue(6);
-        arrayQueue.printAll();
+        CircularArrayQueue<Integer> queue = new CircularArrayQueue<>(5);
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.printAll();
+        print(queue.deQueue());
+        queue.enQueue(5);
+        queue.printAll();
+
     }
 
     private static void print(Object o) {
