39d852e71a28f8b4278cb3164e9dbcaec675e364
huaileicoder <hl_0906n@163.com>
2020-12-08 22:45:03 +0800

    simplify code

--- array/PlusOne.java
+++ array/PlusOne.java
@@ -9,26 +9,15 @@ public class PlusOne {
     }
 
     private static int[] plusOne(int[] digits) {
-        boolean isOverFlow = false;
         for (int i = digits.length - 1; i >= 0; i--) {
-            int digit = digits[i] + 1;
-            if (digit > 9) {
-                digits[i] = 0;
-                if (i == 0) {
-                    isOverFlow = true;
-                }
-            } else {
-                digits[i] = digit;
-                break;
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
+            digits[i] = 0;
         }
-        if (isOverFlow) {
-            digits[0] = 0;
-            int[] newDigits = new int[digits.length + 1];
-            newDigits[0] = 1;
-            System.arraycopy(digits, 0, newDigits, 1, digits.length);
-            digits = newDigits;
-        }
-        return digits;
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
     }
 }
